2a4b702c8f154a1c73308e41e9dae525504c6090
Harsh Athavale <harsh.autowebbed@gmail.com>
2024-01-03 17:50:26 +0530

    Added: Thank You Page

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@
 Fancybox.bind('[data-fancybox="gallery"]', {
   // Custom options for the first gallery
 });
+
+// Popup Auto
+$(window).ready(function () {
+  setTimeout(function () {
+    $("#popupModal").modal("show");
+  }, 3000);
+});
